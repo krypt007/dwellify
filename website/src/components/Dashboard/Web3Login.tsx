ab05976350c1100435777c45c01690d71d020e6b
krypt007 <steve@calstec.com>
2024-08-14 04:05:12 -0400

    update background

--- website/src/components/Dashboard/Web3Login.tsx
+++ website/src/components/Dashboard/Web3Login.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Header from '@/components/Header';
 import Link from 'next/link';
+import Image from 'next/image';
 import { ConnectButton, } from "thirdweb/react";
 import {client} from '@/app/client'
 
@@ -29,6 +30,16 @@ export default function Web3Login() {
     <div  className="bg-[url('/images/login_bg.jpeg')] bg-cover bg-center h-screen flex items-center justify-center">
       <div className="min-h-screen flex flex-col items-center justify-between">
         <Header />
+        <div className='text-yellow-700 text-xl mb-32'>
+          <Image
+            src="/logo.png"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-1 h-32 w-fit"
+          />
+          <div className='flex justify-center text-xl text-yellow-700 font-bold'>DWELIFY</div>
+        </div>
         <div className='flex p-3'>
         <h1 className="text-white text-3xl font-bold mb-4 p-3">Find Your Perfect Place, Effortlessly!</h1>
         </div>
