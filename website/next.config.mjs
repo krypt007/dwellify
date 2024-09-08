7d05b90f433b02876d6c315e212d22c2687e2d4b
krypt007 <steve@calstec.com>
2024-09-08 09:59:59 -0400

    pino

--- website/next.config.mjs
+++ website/next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    serverExternalPackages: ['sequelize', 'pino', 'pino-pretty'],
+};
 
 export default nextConfig;
