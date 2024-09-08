cef2f683f5360b880c6987237003047c33a1f834
krypt007 <steve@calstec.com>
2024-09-08 10:26:39 -0400

    removed seed

--- website/src/app/api/seed/update/route.ts
+++ website/src/app/api/seed/update/route.ts
@@ -2,10 +2,14 @@ import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  // await prisma.property.updateMany({
-  //   data: {
-  //     userId: "kp_3f9629c74528470786077c66746aa7de",
-  //   },
-  // });
-  return NextResponse.json({ success: true });
+  try {
+    // await prisma.property.updateMany({
+    //   data: {
+    //     userId: "kp_3f9629c74528470786077c66746aa7de",
+    //   },
+    // });
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    return NextResponse.error;
+  }
 }
